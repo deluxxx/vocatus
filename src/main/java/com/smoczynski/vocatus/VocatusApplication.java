89f14e8077bbaf34127b7d28e89f1c5a275c94f9
Mirosław Smoczyński <miroslaw.smoczynski@circlekeurope.com>
2019-05-06 11:23:53 +0200

    init

---
+++ src/main/java/com/smoczynski/vocatus/VocatusApplication.java
@@ -0,0 +1,13 @@
+package com.smoczynski.vocatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocatusApplication.class, args);
+	}
+
+}
